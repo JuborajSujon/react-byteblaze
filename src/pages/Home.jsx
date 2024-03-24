ce6131d4d23d62bfb4bbf6841835df1ffa9847f5
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-24 14:10:42 +0600

    animated gradient text with tailwindcss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import Wave from "../assets/wave.svg";
 
 const Home = () => {
   return (
-    <div className="min-h-[calc(100vh-116px)] flex flex-col justify-center items-center">
+    <div className="relative min-h-[calc(100vh-116px)] flex flex-col justify-center items-center">
       <Hero />
+      <img src={Wave} alt="wave" className="absolute bottom-0 w-full" />
     </div>
   );
 };
