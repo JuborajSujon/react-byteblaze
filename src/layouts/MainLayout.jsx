23def9ff3290cb5630a0bd7000544257246801fe
Juboraj Sujon <mdsujon72@gmail.com>
2024-03-24 13:20:35 +0600

    use footer from daisyUI

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
@@ -8,6 +9,8 @@ const MainLayout = () => {
         <Nav />
       </div>
       <Outlet />
+      {/* Footer */}
+      <Footer />
     </div>
   );
 };
